bd17cbd5a415c55599aac75cd4e5560ba43754fa
Acheul <seoul605.21@gmail.com>
2024-07-23 14:07:28 +0900

    0.2.0

--- seoul/src/isomorphism.rs
+++ seoul/src/isomorphism.rs
@@ -2,11 +2,79 @@ pub use seoul_derive::Isomorphism;
 
 /// Trait Isomorphism
 /// 
-/// Using derive macro, you can implement trait Into<T> for Self and &Self, and From<T> for Self too.
+/// * Using derive macro, you can implement
+///   * trait `Isomorphism`
+///     - `fn title(&self) -> &str;`
+///     - `fn list() -> Vec<Self>;`
+///   * trait `Into<T>` for `&Self` and `Self`
+///   * trait `From<T>` and `From<&T>` for `Self`, when the type implements trait `Default`
+/// 
+/// * Works only on `Enum` type
+/// 
+/// # derive Syntax
+/// * When **title** is not given at variant level, **the variant's name (Ident)** will be used as titile.
+/// * When **list** is not given at the top level attribute, list of each variant's default format will be returned.
+/// * When **into** value is not given at variant level, the into type(T)'s default value will be used in `<Into<T>>` trait.
+/// * When the type implements `Default` and **has_default** is given at top level attribute, the `From<T>` and `From<&T>` will be implemented for each given into types(T). 
+/// 
+/// # Ex
+/// ```
+/// # use seoul::Isomorphism;
+/// 
+/// #[derive(PartialEq, Debug, Isomorphism)]
+/// #[isomorphism(u8, list=[A, B(10)])]
+/// enum ABC {
+///   #[into(0)] #[title("A")] A,
+///   #[into(1)] B(i32),
+///   C,
+/// }
+///
+/// // `list()`
+/// let list = ABC::list();
+/// assert_eq!(list, vec![ABC::A, ABC::B(10)]);
+/// 
+/// let a: ABC = ABC::A;
+/// let b = ABC::B(10);
+/// let c = ABC::C;
+/// 
+/// // `title()`
+/// assert_eq!(a.title(), "A");
+/// assert_eq!(b.title(), "B");
+/// assert_eq!(c.title(), "C");
+/// 
+/// // `Into<T>` for `&Self` and `Self`
+/// assert_eq!(Into::<u8>::into(&a), 0);
+/// assert_eq!(Into::<u8>::into(a), 0);
+/// assert_eq!(Into::<u8>::into(&b), 1);
+/// assert_eq!(Into::<u8>::into(c), 0);
+/// 
+/// 
+/// #[derive(Default, Debug, Isomorphism, PartialEq)]
+/// #[isomorphism(into=[u8, i8], has_default)]
+/// pub enum CD {
+///   #[default] #[into([0, 1])] C,
+///   #[into([0, -1])] D,
+/// }
+/// 
+/// // list
+/// assert_eq!(CD::list(), vec![CD::C, CD::D]);
+/// 
+/// // Into
+/// assert_eq!(Into::<u8>::into(CD::C), 0);
+/// assert_eq!(Into::<i8>::into(CD::C), 1);
+/// assert_eq!(Into::<u8>::into(CD::D), 0);
+/// assert_eq!(Into::<i8>::into(CD::D), -1);
+/// 
+/// // From
+/// assert_eq!(Into::<CD>::into(1i8), CD::C);
+/// assert_eq!(Into::<CD>::into(-1i8), CD::D);
+/// // fallback to default value of `CD`
+/// assert_eq!(Into::<CD>::into(-0i8), CD::C);
+/// ```
 /// 
 pub trait Isomorphism: Sized {
 
-  fn title(&self) -> String;
+  fn title(&self) -> &str;
 
   fn list() -> Vec<Self>;
 }
--- seoul/src/test.rs
+++ seoul/src/test.rs
@@ -1,96 +1,89 @@
 #[cfg(test)]
-mod tests {
-  use crate::*;
+use crate::*;
 
-  #[test]
-  fn test_isomorphsim1() {
+#[test]
+fn test_isomorphsim1() {
 
-    #[derive(PartialEq, Debug, Default, Isomorphism)]
-    #[isomorphism(u8, list=[A, B(10)])]
-    enum ABC {
-      #[default] #[title("A")] A,
-      #[into(1)] #[title("B")] B(i32),
-      C,
-    }
+  #[derive(PartialEq, Debug, Isomorphism)]
+  #[isomorphism(u8, list=[A, B(10)])]
+  enum ABC {
+    #[into(0)] #[title("A")] A,
+    #[into(1)] B(i32),
+    C,
+  }
 
-    let list = ABC::list();
-    assert_eq!(list, vec![ABC::A, ABC::B(10)]);
+  // `list()`
+  let list = ABC::list();
+  assert_eq!(list, vec![ABC::A, ABC::B(10)]);
+
+  let a: ABC = ABC::A;
+  let b = ABC::B(10);
+  let c = ABC::C;
+
+  // `title()`
+  assert_eq!(a.title(), "A");
+  assert_eq!(b.title(), "B");
+  assert_eq!(c.title(), "C");
+
+  // `Into<T>` for `&Self` and `Self`
+  assert_eq!(Into::<u8>::into(&a), 0);
+  assert_eq!(Into::<u8>::into(a), 0);
+  assert_eq!(Into::<u8>::into(&b), 1);
+  assert_eq!(Into::<u8>::into(c), 0);
+
+
+  // `has_default` enables `From<T>` and `From<&T>` trait for `Self`
+  #[derive(Default, PartialEq, Debug, Isomorphism)]
+  #[isomorphism(bool, has_default)]
+  enum DEF {
+    #[default] D,
+    #[into(true)]
+    E {
+      e1: i32, e2: Vec<usize>
+    },
+    F(bool, usize)
+  }
+
+  let d = DEF::D;
+  let e = DEF::E { e1: 10, e2: vec![1, 2, 3 ] };
+  let f = DEF::F(true, 100);
   
-    let a = ABC::A;
-    let b = ABC::B(10);
-    let c = ABC::C;
+  // Into<bool>
+  assert!(!Into::<bool>::into(&d));
+  assert!(Into::<bool>::into(&e));
+  assert!(!Into::<bool>::into(f));
 
-    // Into
-    assert_eq!(Into::<u8>::into(&a), 0);
-    assert_eq!(Into::<u8>::into(a), 0);
-    assert_eq!(Into::<u8>::into(&b), 1);
-    assert_eq!(b.title().as_str(), "B");
-    assert_eq!(c.title().as_str(), "C");
+  // From<bool>
+  let t: DEF = true.into();
+  let f: DEF = false.into();
+  assert!(t==DEF::E{e1:0, e2:Vec::<usize>::new()});
+  assert!(f==DEF::default());
 
-    // From
-    let x: ABC = 0u8.into();
-    assert!(x==ABC::A);
-    let x: ABC = 1u8.into();
-    assert!(x==ABC::B(0));
-    let x: ABC = 20u8.into();
-    assert!(x==ABC::A);
+  let list = DEF::list();
+  assert_eq!(list, vec![DEF::D, DEF::E { e1: 0, e2: Vec::<usize>::new()}, DEF::F(false, 0)]);
+}
 
-    
-    #[derive(PartialEq, Debug,Default, Isomorphism)]
-    #[isomorphism(bool)]
-    enum DEF {
-      #[default] D,
-      #[into(true)]
-      E {
-        e1: i32, e2: Vec<usize>
-      },
-      F(bool, usize)
-    }
+#[test]
+fn test_isomorphsim2() {
 
-    let d = DEF::D;
-    let e = DEF::E { e1: 10, e2: vec![1, 2, 3 ] };
-    let f = DEF::F(true, 100);
-    
-    // Into<bool>
-    assert!(!Into::<bool>::into(&d));
-    assert!(Into::<bool>::into(&e));
-    assert!(!Into::<bool>::into(f));
+  // use alias type to prevent derive macro's parsing error when the type's raw name has white space;
+  type StaticStr = &'static str;
 
-    // From<bool>
-    let t: DEF = true.into();
-    let f: DEF = false.into();
-    assert!(t==DEF::E{e1:0, e2:Vec::<usize>::new()});
-    assert!(f==DEF::D);
-
-    let list = DEF::list();
-    assert_eq!(list, vec![DEF::D, DEF::E { e1: 0, e2: Vec::<usize>::new()}, DEF::F(false, 0)]);
+  #[derive(Debug, PartialEq, Isomorphism)]
+  #[isomorphism(into=[u8, i8, StaticStr])]
+  pub enum CD {
+    #[into([0, 1, "c"])] C,
+    #[into([0, -1, "d"])] D,
   }
 
-  #[test]
-  fn test_isomorphsim2() {
+  let c = CD::C;
+  let d = CD::D;
 
-    // use alias type to prevent derive macro's parsing error.
-    type StaticStr = &'static str;
+  assert_eq!(Into::<u8>::into(&c), 0);
+  assert_eq!(Into::<i8>::into(&c), 1);
+  assert_eq!(Into::<u8>::into(&d), 0);
+  assert_eq!(Into::<i8>::into(&d), -1);
 
-    #[derive(Debug, PartialEq, Default, Isomorphism)]
-    #[isomorphism(into=[u8, i8, StaticStr])]
-    pub enum CD {
-      #[default] #[into([0, 1, "c"])] C,
-      #[into([0, -1, "d"])] D,
-    }
-
-    let c = CD::C;
-    let d = CD::D;
-
-    assert_eq!(Into::<u8>::into(&c), 0);
-    assert_eq!(Into::<i8>::into(&c), 1);
-    assert_eq!(Into::<u8>::into(&d), 0);
-    assert_eq!(Into::<i8>::into(&d), -1);
-
-    assert_eq!(Into::<CD>::into(1i8), CD::C);
-    assert_eq!(Into::<CD>::into(-1i8), CD::D);
-
-    let c: &str = c.into();
-    assert_eq!(c, "c");
-  }
+  let c: &str = c.into();
+  assert_eq!(c, "c");
 }
